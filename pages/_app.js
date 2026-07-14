581cd97e61100d8c54c471d47e474b753e99331c
CenturionWho <johnathan@armortechrepair.com>
2026-07-14 06:08:45 -0500

    Show ArmorTech chat on every Solutions page

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,21 @@
 import '../styles/globals.css';
+import ChatLauncher from '../components/ChatLauncher';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-
+      <ChatLauncher />
       <address
         itemScope
         itemType="https://schema.org/PostalAddress"
-        style={{
-          margin: 0,
-          padding: '18px 20px',
-          textAlign: 'center',
-          fontStyle: 'normal',
-          lineHeight: 1.65,
-          borderTop: '1px solid rgba(255,255,255,0.16)',
-          background: '#050505',
-          color: '#cfcfcf',
-        }}
+        style={{ margin: 0, padding: '18px 20px', textAlign: 'center', fontStyle: 'normal', lineHeight: 1.65, borderTop: '1px solid rgba(255,255,255,0.16)', background: '#050505', color: '#cfcfcf' }}
       >
         <strong style={{ color: '#ff7a1a' }}>ArmorTech Headquarters:</strong>{' '}
-        <a
-          href="https://www.google.com/maps/search/?api=1&query=719+Hope+Rd%2C+Stamps%2C+AR+71860"
-          style={{ color: 'inherit' }}
-        >
-          <span itemProp="streetAddress">719 Hope Rd</span>,{' '}
-          <span itemProp="addressLocality">Stamps</span>,{' '}
-          <span itemProp="addressRegion">AR</span>{' '}
-          <span itemProp="postalCode">71860</span>
+        <a href="https://www.google.com/maps/search/?api=1&query=719+Hope+Rd%2C+Stamps%2C+AR+71860" style={{ color: 'inherit' }}>
+          719 Hope Rd, Stamps, AR 71860
         </a>
-        <br />
-        Serving Arkansas, Texas, Louisiana, Oklahoma, and Mississippi.
+        <br />Serving Arkansas, Texas, Louisiana, Oklahoma, and Mississippi.
       </address>
     </>
   );
