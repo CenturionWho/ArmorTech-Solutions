995fde039db4b64fd35dcf841ec80e0b79428e60
Johnathan Chambers <johnathan@armortechrepair.com>
2026-07-15 17:06:20 -0500

    Add Google Analytics to ArmorTech Solutions

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
-import '../styles/globals.css';
-import ChatLauncher from '../components/ChatLauncher';
+import "../styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <ChatLauncher />
-      <address
-        itemScope
-        itemType="https://schema.org/PostalAddress"
-        style={{ margin: 0, padding: '18px 20px', textAlign: 'center', fontStyle: 'normal', lineHeight: 1.65, borderTop: '1px solid rgba(255,255,255,0.16)', background: '#050505', color: '#cfcfcf' }}
-      >
-        <strong style={{ color: '#ff7a1a' }}>ArmorTech Headquarters:</strong>{' '}
-        <a href="https://www.google.com/maps/search/?api=1&query=719+Hope+Rd%2C+Stamps%2C+AR+71860" style={{ color: 'inherit' }}>
-          719 Hope Rd, Stamps, AR 71860
-        </a>
-        <br />Serving Arkansas, Texas, Louisiana, Oklahoma, and Mississippi.
-      </address>
+      <GoogleAnalytics gaId="G-66K2BF9FTW" />
     </>
   );
 }
